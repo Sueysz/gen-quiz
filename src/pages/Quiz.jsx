c80bde84d94f5c4cbcbb0647f6eddd8a35c74839
sueys <sueys@live.fr>
2023-05-17 10:48:02 +0200

    affichage du titre du quiz

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,11 +1,8 @@
-// import { getQuiz } from "../api";
-// import { useParams } from "react-router-dom"
-
+import './Quiz.css'
+import { listQuiz, getQuiz } from '../api'
 import { useState } from "react"
 import { Link } from 'react-router-dom'
-import './Quiz.css'
 import { fireConfetti } from "../utils/fireConffeti"
-import { getQuiz } from "../api"
 
 
 const questions = [
@@ -28,10 +25,10 @@ const questions = [
         solution: 1
     },
     {
-        question: 'sun préfére quel endroit pour ces vacances ?⛷️',
+        question: 'sun préfère quel endroit pour ses vacances ?⛷️',
         answers: [
             'Sa chambre',
-            "L'angletère",
+            "L'Angleterre",
             'Lille'
         ],
         solution: 0
@@ -43,45 +40,58 @@ export const Quiz = () => {
     const [selectedAnswer, setSelectedAnswer] = useState()
     const [questionsIndex, setQuestionsIndex] = useState(0)
     const question = questions[questionsIndex]
-    
+    const quizzes = listQuiz();
+    const quiz = getQuiz(quizzes[questionsIndex].slug)
 
     const checkAnswer = () => {
         if (question.solution === selectedAnswer) {
             fireConfetti();
             setQuestionsIndex((i) => i + 1)
         } else {
-            alert('oups recommence 😓')
+            alert('Oups, recommencez 😓')
             setQuestionsIndex(0)
         }
-
     }
+
     if (question === undefined) {
-        return <>
-            <div>bravo <button ><Link to="/">Retour au Quiz</Link></button></div>
-        </>
+        return (
+            <>
+                <div>
+                    Bravo! <button><Link to="/">Retour au Quiz</Link></button>
+                </div>
+            </>
+        )
     }
 
-    return <>
-        <div>
-            <button>
-                Back ←
-            </button>
-
-            <h1></h1>
-        </div>
-        <div id="question">
-            <h1>{question.question}</h1>
-            <div id="answers">
-                {question.answers.map((btn, i) => {
-                    return <button key={btn} className={i === selectedAnswer ? 'answer selected' : 'answer'} onClick={() => setSelectedAnswer(i)}>{i + 1}. {btn}</button>
-                })}
-                <div id="confirm">
-                    <button disabled={selectedAnswer === undefined} onClick={checkAnswer}>
-                        Confirm
-                    </button>
+    return (
+        <>
+            <div>
+                <button>
+                    Back ←
+                </button>
+                <h1>{quiz.title}</h1>
+            </div>
+            <div id="question">
+                <h1>{question.question}</h1>
+                <div id="answers">
+                    {question.answers.map((btn, i) => {
+                        return (
+                            <button
+                                key={btn}
+                                className={i === selectedAnswer ? 'answer selected' : 'answer'}
+                                onClick={() => setSelectedAnswer(i)}
+                            >
+                                {i + 1}. {btn}
+                            </button>
+                        )
+                    })}
+                    <div id="confirm">
+                        <button disabled={selectedAnswer === undefined} onClick={checkAnswer}>
+                            Confirm
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-
+        </>
+    )
 }
