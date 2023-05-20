48ffb0feed79b25d2a0b245d3cec18f260f64b93
sueys <sueys@live.fr>
2023-05-20 20:37:42 +0200

    remove hard coded data  and add fetch to db

--- src/api.js
+++ src/api.js
@@ -1,13 +1,3 @@
-const data = [
-    {id: 0,slug:'premier',title: 'Premier Quiz',color:'rgba(104, 219, 255, 1)'},
-    {id: 1,slug:'deuxieme',title: 'Deuxième Quiz',color:'rgba(185, 117, 239, 1)'},
-    {id: 2,slug:'troisieme',title: 'Troisième Quiz',color:'rgba(252, 129, 77, 1)'},
-    {id: 3,slug:'quatrieme',title: 'Quatrième Quiz',color:'rgba(255, 213, 64, 1)'},
-    {id: 4,slug:'cinquieme',title: 'Cinquième Quiz',color:'rgba(255, 88, 238, 1)'},
-    {id: 5,slug:'sixieme',title: 'Sixième Quiz',color:'rgba(42, 237, 120, 1)'},
-]
-
-
 
 export const login = (email,password)=>{
     console.log(email,password)
@@ -19,9 +9,14 @@ export const getAuth = () =>{
     return {username:"stormy"}
 }
 export const listQuiz = ()=>{
-    return data;
+    return fetch("http://localhost:8800/quiz").then((res)=>{
+        return res.json()
+    })
 };
 export const getQuiz = (slug)=>{
-    return data.find(quiz =>quiz.slug === slug )
+    // return data.find(quiz =>quiz.slug === slug )
+    return fetch(`http://localhost:8800/quiz/${slug}`).then((res)=>{
+        return res.json()
+    })
 }
 
