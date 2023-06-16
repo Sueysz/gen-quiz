23407872b14709112d1f72db48f584888f016323
sueys <sueys@live.fr>
2023-06-16 11:04:10 +0200

    creat component for homepage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled"
+
+export const HomePage = styled.div`
+    h1{
+        text-align:center;
+    }
+
+    .wrapper{
+        display:flex;
+        justify-content:space-between;
+    }
+    
+    .grid{
+        gap: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+`
