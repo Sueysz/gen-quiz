426ea90ea3a42a47a71fac6816384301824fb3f6
sueys <sueys@live.fr>
2023-06-20 17:34:47 +0200

    styled formQuiz

---
+++ src/components/FormPage.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled"
+
+export const FormPage = styled.div`
+
+    h1{
+        text-align:center;
+    }
+
+    form{
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+    }
+
+    input{
+        width: 30vw;
+        height: 7vh;
+        border: 2px solid #222;
+        border-radius: 5px;
+        padding-left:15px;
+    }
+
+    
+`
