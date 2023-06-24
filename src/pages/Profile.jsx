b41f1b821e678e45b51632de585f1324a8fd803c
sueys <sueys@live.fr>
2023-06-24 15:23:07 +0200

    WIP profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useAuth } from '../utils/AuthProvider';
 
 export const Profile = () => {
+    const { user } = useAuth();
+    console.log(user)
+
+    if (!user) {
+        return <p>Loading...</p>;
+    }
+
     return (
-        <div>Profile</div>
-    )
-}
+        <div>
+            <h1>Hey {user.username}!</h1>
+            <p>Email: {user.email}</p>
+        </div>
+    );
+};
