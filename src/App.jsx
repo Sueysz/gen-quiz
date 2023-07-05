fea19bac365ccfd2781009c552d0f3a02b4f5838
sueys <sueys@live.fr>
2023-07-06 00:35:02 +0200

    remove css

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,22 @@ import { Register } from './pages/Register';
 import { NotFound } from './pages/NotFound';
 import { Routes, Route } from "react-router-dom";
 
-import './App.css'
 import { FormQuiz } from './pages/FormQuiz';
 
 const App = () => {
 
   return (
     <>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path='quiz/:slug' element={<Quiz />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path='test' element={<Test />} />
-          <Route path='FormQuiz' element={<FormQuiz />} />
-          <Route path='Profile' element={<Profile />} />
-        </Routes>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path='quiz/:slug' element={<Quiz />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path='test' element={<Test />} />
+        <Route path='FormQuiz' element={<FormQuiz />} />
+        <Route path='Profile' element={<Profile />} />
+      </Routes>
     </>
   )
 }
