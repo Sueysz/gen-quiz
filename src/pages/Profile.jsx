7abed6111076612857c0c3d67e86a0036bb00950
sueys <sueys@live.fr>
2023-07-09 00:31:39 +0200

    fetch user info and profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../utils/AuthProvider';
+import { FetchUserInfo } from '../api';
 
 export const Profile = () => {
-    const { user } = useAuth();
+    const { isLoggedIn } = useAuth();
+    const [user, setUser] =useState([]);
 
-    if (!user) {
-        return <p>Loading...</p>;
-    }
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const userData = await FetchUserInfo();
+                setUser(userData)
+            } catch (error) {
+                console.log('Error fetching user information:', error);
+            }
+        };
+        if (isLoggedIn) {
+            fetchUser();
+        }
+    }, [isLoggedIn]);
 
     return (
         <div>
-            <h1>Hey {user.username}!</h1>
-            <p>Email: {user.email}</p>
+            {isLoggedIn ? (
+                user ?(
+                    <div>
+                        <h1>Welcome to your profile, {user.username}!</h1>
+                        <p>Email: {user.email}</p>
+                    </div>
+                ) : (
+                    <p>Loading User data...</p>
+                )
+            ) : (
+                <h1>Please log in to view your profile.</h1>
+            )}
         </div>
     );
 };
