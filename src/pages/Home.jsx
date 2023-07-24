71f36229a8acf164bbf80b670aa5e56382c9d11a
sueys <sueys@live.fr>
2023-07-24 02:52:29 +0200

    add categories fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import { Logout } from '../components/Logout';
 import { useAuth } from '../utils/AuthProvider';
 import { HomePage } from '../components/HomePage';
-import { listQuiz } from '../api';
+import { FetchCategories, listQuiz } from '../api';
 import { LinkButton } from '../components/Buttons';
 import { StyledIcon } from '../components/Icons';
 import { Card, CardAdd } from '../components/Card';
@@ -10,10 +10,14 @@ import { useEffect, useState } from 'react';
 
 export const Home = () => {
     const [quizList, setQuizList] = useState([]);
+    const [categoriesList,setCategoriesList] = useState([]);
     const { isLoggedIn } = useAuth();
     useEffect(() => {
         listQuiz().then(setQuizList)
     }, [])
+    useEffect(()=>{
+        FetchCategories().then(setCategoriesList)
+    },[])
 
     return <>
         <HomePage>
@@ -34,13 +38,16 @@ export const Home = () => {
                 </div>
             </div>
             <h1>👇Choose Your Quiz👇</h1>
+            {categoriesList.map(categorie =>(
+                <p key={categorie.id}>{categorie.name}</p>
+            ))}
 
             <div className='grid'>
                 {isLoggedIn ? (
                     <CardAdd style={{ backgroundColor: 'black' }} to="/FormQuiz"> </CardAdd>
                 ) : null}
                     {quizList.map(quiz => (
-                        <Card to={`quiz/${quiz.slug}`} style={{ backgroundColor: quiz.color }} key={quiz.id}>
+                        <Card to={`quiz/${quiz.id}`} style={{ backgroundColor: quiz.color }} key={quiz.id}>
                             <p>{quiz.title}</p>
                         </Card>
                     ))}
