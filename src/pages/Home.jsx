58ebae91f107f9e0068e44edf2f015be2b4c812d
sueys <sueys@live.fr>
2023-08-01 02:15:15 +0200

    add promiseall of all api and create filter for category match with category_quiz

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
-
 import { Logout } from '../components/Logout';
 import { useAuth } from '../utils/AuthProvider';
 import { HomePage } from '../components/HomePage';
-import { fetchCategories, listQuiz } from '../api';
+import { fetchCategories, getQuizCategories, listQuiz } from '../api';
 import { LinkButton } from '../components/Buttons';
 import { StyledIcon } from '../components/Icons';
 import { Card, CardAdd } from '../components/Card';
@@ -11,15 +10,41 @@ import { useEffect, useState } from 'react';
 export const Home = () => {
     const [quizList, setQuizList] = useState([]);
     const [categoriesList, setCategoriesList] = useState([]);
+    const [categoriesQuiz, setCategoriesQuiz] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [filteredQuizList, setFilteredQuizList] = useState([]);
     const { isLoggedIn } = useAuth();
-    useEffect(() => {
-        listQuiz().then(setQuizList)
-    }, [])
-    useEffect(() => {
-        fetchCategories().then(setCategoriesList)
-    }, [])
 
-    return <>
+    useEffect(() => {
+        Promise.all([listQuiz(), fetchCategories(), getQuizCategories()])
+            .then(([quizData, categoriesData, categoriesQuizData]) => {
+                setQuizList(quizData);
+                setCategoriesList(categoriesData);
+                setCategoriesQuiz(categoriesQuizData);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (selectedCategory === '') {
+            setFilteredQuizList(quizList);
+        } else {
+            const filteredQuizzes = quizList.filter((quiz) =>
+                categoriesQuiz.some(
+                    (category) => category.quiz_id === quiz.id && category.category_id === selectedCategory
+                )
+            );
+            setFilteredQuizList(filteredQuizzes);
+        }
+    }, [selectedCategory, categoriesQuiz, quizList]);
+
+    const handleCategorySelection = (categoryId) => {
+        setSelectedCategory(categoryId);
+    };
+
+    return (
         <HomePage>
             <div className='wrapper'>
                 <div>
@@ -29,30 +54,30 @@ export const Home = () => {
                     {isLoggedIn ? (
                         <>
                             <Logout />
-                            <LinkButton to="/profile"> Profile</LinkButton>
+                            <LinkButton to='/profile'>Profile</LinkButton>
                         </>
                     ) : (
-                        <LinkButton to="/login">LogIn </LinkButton>
+                        <LinkButton to='/login'>LogIn</LinkButton>
                     )}
-
                 </div>
             </div>
             <h1>👇Choose Your Quiz👇</h1>
-            {categoriesList.map(categorie => (
-                <p key={categorie.id}>{categorie.name}</p>
-            ))}
+            <div className='grid'>
+                {categoriesList.map((categorie) => (
+                    <button onClick={() => handleCategorySelection(categorie.id)} key={categorie.id}>
+                        {categorie.name}
+                    </button>
+                ))}
+            </div>
 
             <div className='grid'>
-                {isLoggedIn ? (
-                    <CardAdd style={{ backgroundColor: 'black' }} to="/FormQuiz"> </CardAdd>
-                ) : null}
-                {quizList.map(quiz => (
+                {isLoggedIn ? <CardAdd style={{ backgroundColor: 'black' }} to='/FormQuiz' /> : null}
+                {filteredQuizList.map((quiz) => (
                     <Card to={`quiz/${quiz.id}`} style={{ backgroundColor: quiz.color }} key={quiz.id}>
                         <p>{quiz.title}</p>
                     </Card>
                 ))}
             </div>
         </HomePage>
-    </>
-
-}
+    );
+};
