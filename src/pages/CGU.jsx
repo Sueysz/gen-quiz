b5948ebd2cbb722c52973a8769eddb69f6828c03
sueys <sueys@live.fr>
2023-08-08 16:50:04 +0200

    style for cgu

--- src/pages/CGU.jsx
+++ src/pages/CGU.jsx
@@ -5,49 +5,61 @@ export const CGU = () => {
     return (<>
         <div>
 
-            Conditions Générales d'Utilisation (CGU) pour un site de quiz :
+            <h1>Conditions Générales d'Utilisation (CGU) pour un site de quiz :</h1>
 
-            Date d'effet : [08/08/2023]
+            <h2>Date d'effet : [08/08/2023]</h2>
 
-            Bienvenue sur [GenQuiz] ! Avant d'utiliser ce site, veuillez lire attentivement les présentes Conditions Générales d'Utilisation (ci-après "CGU"). En accédant ou en utilisant le site de quiz, vous acceptez d'être lié(e) par ces CGU. Si vous n'acceptez pas ces CGU, veuillez ne pas utiliser le site.
+            <h3>Bienvenue sur [GenQuiz] ! Avant d'utiliser ce site, veuillez lire attentivement les présentes Conditions Générales d'Utilisation (ci-après "CGU"). En accédant ou en utilisant le site de quiz, vous acceptez d'être lié(e) par ces CGU. Si vous n'acceptez pas ces CGU, veuillez ne pas utiliser le site.</h3>
 
-            Définitions
-            "Site" désigne [GenQuiz], accessible via l'URL [Adresse du site].
-            "Utilisateur" désigne toute personne accédant au site et/ou utilisant ses fonctionnalités.
-            "Quiz" désigne les différents questionnaires interactifs disponibles sur le site.
-            Utilisation du Site
-            a. Éligibilité : L'utilisation du site est ouverte aux Utilisateurs âgés de [+12] et plus. Les mineurs doivent obtenir l'autorisation de leurs parents ou tuteurs avant d'utiliser le site.
+            <h2>Définitions</h2>
+            <p>"Site" désigne [GenQuiz], accessible via l'URL [Adresse du site].
+                "Utilisateur" désigne toute personne accédant au site et/ou utilisant ses fonctionnalités.
+                "Quiz" désigne les différents questionnaires interactifs disponibles sur le site.
+                Utilisation du Site
+            </p>
+            <ul>
+                <li>a. Éligibilité : L'utilisation du site est ouverte aux Utilisateurs âgés de [+12] et plus. Les mineurs doivent obtenir l'autorisation de leurs parents ou tuteurs avant d'utiliser le site.</li>
 
-            b. Compte Utilisateur : Certains aspects du site peuvent nécessiter la création d'un compte utilisateur. Les informations fournies lors de l'inscription doivent être exactes, complètes et à jour. Vous êtes responsable de la confidentialité de votre compte et de vos informations d'identification. Toute activité effectuée sous votre compte est de votre responsabilité.
+                <li>b. Compte Utilisateur : Certains aspects du site peuvent nécessiter la création d'un compte utilisateur. Les informations fournies lors de l'inscription doivent être exactes, complètes et à jour. Vous êtes responsable de la confidentialité de votre compte et de vos informations d'identification. Toute activité effectuée sous votre compte est de votre responsabilité.</li>
 
-            c. Utilisation Légitime : Vous vous engagez à utiliser le site uniquement à des fins légales et conformes à ces CGU. Toute utilisation abusive, frauduleuse ou illégale du site est strictement interdite.
+                <li>c. Utilisation Légitime : Vous vous engagez à utiliser le site uniquement à des fins légales et conformes à ces CGU. Toute utilisation abusive, frauduleuse ou illégale du site est strictement interdite.</li>
 
-            d. Contenu de l'Utilisateur : En utilisant le site, vous pouvez soumettre du contenu, tel que des réponses à des quiz, des commentaires, etc. Vous conservez les droits sur votre contenu, mais vous accordez à [GenQuiz] une licence mondiale, non-exclusive, libre de droits, pour utiliser, afficher, reproduire et distribuer votre contenu sur le site.
+                <li>d. Contenu de l'Utilisateur : En utilisant le site, vous pouvez soumettre du contenu, tel que des réponses à des quiz, des commentaires, etc. Vous conservez les droits sur votre contenu, mais vous accordez à [GenQuiz] une licence mondiale, non-exclusive, libre de droits, pour utiliser, afficher, reproduire et distribuer votre contenu sur le site.</li>
+            </ul>
 
-            e. Restrictions d'utilisation : Vous acceptez de ne pas utiliser le site pour :
 
-            Diffuser du contenu offensant, illégal, diffamatoire, obscène, pornographique ou autrement répréhensible.
-            Porter atteinte aux droits de propriété intellectuelle d'autrui.
-            Perturber le fonctionnement du site, introduire des virus ou tout autre code nuisible.
-            Collecter des informations personnelles d'autres Utilisateurs sans leur consentement.
-            Engager toute activité qui viole ces CGU ou toute loi applicable.
-            Propriété Intellectuelle
-            Tous les contenus présents sur le site, y compris les quiz, textes, images, logos, marques, etc., sont la propriété exclusive de [GenQuiz] ou de ses partenaires. Ils sont protégés par les lois sur la propriété intellectuelle en vigueur. Toute utilisation non autorisée de ces contenus est strictement interdite.
 
-            Modifications du Site et des CGU
-            [GenQuiz] se réserve le droit de modifier, suspendre ou interrompre le site ou une partie de ses fonctionnalités à tout moment, sans préavis. Les CGU peuvent également être mises à jour de temps en temps. Vous serez informé(e) des modifications substantielles des CGU avant leur entrée en vigueur.
+            <h2>Restrictions d'utilisation : Vous acceptez de ne pas utiliser le site pour :</h2>
 
-            Responsabilité et Limitation de Responsabilité
-            a. Contenu Externe : Le site peut contenir des liens vers des sites tiers. [GenQuiz] n'est pas responsable du contenu, de la sécurité ou de la confidentialité de ces sites.
+            <p>Diffuser du contenu offensant, illégal, diffamatoire, obscène, pornographique ou autrement répréhensible.
+                Porter atteinte aux droits de propriété intellectuelle d'autrui.
+                Perturber le fonctionnement du site, introduire des virus ou tout autre code nuisible.
+                Collecter des informations personnelles d'autres Utilisateurs sans leur consentement.
+                Engager toute activité qui viole ces CGU ou toute loi applicable.
+                Propriété Intellectuelle
+                Tous les contenus présents sur le site, y compris les quiz, textes, images, logos, marques, etc., sont la propriété exclusive de [GenQuiz] ou de ses partenaires. Ils sont protégés par les lois sur la propriété intellectuelle en vigueur. Toute utilisation non autorisée de ces contenus est strictement interdite.
+            </p>
 
-            b. Garanties : Le site est fourni "tel quel" et [GenQuiz] ne fournit aucune garantie quant à son fonctionnement, sa disponibilité, son exactitude ou son adéquation à un usage spécifique.
+            <h3>
+                Modifications du Site et des CGU
+                [GenQuiz] se réserve le droit de modifier, suspendre ou interrompre le site ou une partie de ses fonctionnalités à tout moment, sans préavis. Les CGU peuvent également être mises à jour de temps en temps. Vous serez informé(e) des modifications substantielles des CGU avant leur entrée en vigueur.
+            </h3>
 
-            c. Responsabilité : [GenQuiz] ne pourra être tenu responsable des dommages directs, indirects, consécutifs ou spéciaux découlant de l'utilisation ou de l'impossibilité d'utiliser le site.
+            <h2>Responsabilité et Limitation de Responsabilité</h2>
+            <ul>
+                <li>a. Contenu Externe : Le site peut contenir des liens vers des sites tiers. [GenQuiz] n'est pas responsable du contenu, de la sécurité ou de la confidentialité de ces sites.</li>
 
-            Loi applicable et Règlement des Litiges
-            Ces CGU sont régies par les lois [""]. Tout litige découlant de l'utilisation du site sera soumis à la compétence exclusive des tribunaux [""].
+                <li> b. Garanties : Le site est fourni "tel quel" et [GenQuiz] ne fournit aucune garantie quant à son fonctionnement, sa disponibilité, son exactitude ou son adéquation à un usage spécifique.</li>
 
-            En acceptant ces CGU, vous déclarez avoir lu, compris et accepté les termes et conditions énoncés dans le présent document. Si vous n'acceptez pas ces CGU, veuillez ne pas utiliser le site.
+                <li>c. Responsabilité : [GenQuiz] ne pourra être tenu responsable des dommages directs, indirects, consécutifs ou spéciaux découlant de l'utilisation ou de l'impossibilité d'utiliser le site.</li>
+            </ul>
+            <h3>
+                Loi applicable et Règlement des Litiges
+            </h3>
+            <p>
+                Ces CGU sont régies par les lois [""]. Tout litige découlant de l'utilisation du site sera soumis à la compétence exclusive des tribunaux [""].
+                En acceptant ces CGU, vous déclarez avoir lu, compris et accepté les termes et conditions énoncés dans le présent document. Si vous n'acceptez pas ces CGU, veuillez ne pas utiliser le site.
+            </p>
         </div>
         <Link to={'/'}>Retour</Link>
     </>
