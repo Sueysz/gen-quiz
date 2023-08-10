1c3c6b4c6a3a3c782910a0bf327f6950641e2853
sueys <sueys@live.fr>
2023-08-10 21:54:57 +0200

    add information users

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,14 +7,20 @@ import { StyledIcon } from '../components/Icons';
 
 export const Profile = () => {
     const { isLoggedIn } = useAuth();
-    const [user, setUser] = useState([]);
-    const [tokenExpired, setTokenExpired] = useState(false); // state pour gérer l'alerte
+    const [user, setUser] = useState({
+        id: 0,
+        username: '',
+        email: ''
+    });
+    const [quiz, setQuiz] = useState([]);
+    const [tokenExpired, setTokenExpired] = useState(false);
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const userData = await fetchUserinfo();
-                setUser(userData);
+                setUser(userData.user);
+                setQuiz(userData.quiz);
             } catch (error) {
                 console.log('Error fetching user information:', error);
                 if (error.response && error.response.status === 401 && error.response.data.message === "Token expired. User has been logged out.") {
@@ -34,15 +40,19 @@ export const Profile = () => {
                     <StyledIcon src='/icons/logo.png' alt='logo' />
                 </Link>
                 {isLoggedIn ? (
-                    user ? (
+                    <div>
+                        {tokenExpired && <p style={{ color: 'red' }}>Votre session a expiré, veuillez vous reconnecter</p>}
+                        <h1>Welcome, {user.username}! 🤹‍♀️</h1>
+                        <p>Email: {user.email}</p>
                         <div>
-                            {tokenExpired && <p style={{ color: 'red' }}>Votre session est expirer, veuillez vous reconnecter</p>}
-                            <h1>Welcome, {user.username}! 🤹‍♀️</h1>
-                            <p>Email: {user.email}</p>
+                            <h2>User Object:</h2>
+                            <pre>{JSON.stringify(user, null, 2)}</pre>
                         </div>
-                    ) : (
-                        <p>Loading User data...</p>
-                    )
+                        <div>
+                            <h2>Quiz Array:</h2>
+                            <pre>{JSON.stringify(quiz, null, 2)}</pre>
+                        </div>
+                    </div>
                 ) : (
                     <h1>Please log in to view your profile. 🤔</h1>
                 )}
