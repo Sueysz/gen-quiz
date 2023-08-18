0d260d223b56461e460e1cf0138c3381194b6769
sueys <sueys@live.fr>
2023-08-18 03:12:42 +0200

    add decoded token and logout auto

--- src/utils/AuthProvider.jsx
+++ src/utils/AuthProvider.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import PropTypes from "prop-types"
+import jwtDecode from "jwt-decode";
 
 // Crée un contexte pour l'authentification
 const AuthContext = createContext();
@@ -7,8 +8,31 @@ const AuthContext = createContext();
 // Crée un fournisseur d'authentification qui encapsule l'état d'authentification
 export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [refreshPage , setRefreshPage] = useState(false);
-    
+    const [refreshPage, setRefreshPage] = useState(false);
+    // Vérifie si le token a expiré avant de rendre le composant
+    const hasTokenExpired = () => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            const decodedToken = jwtDecode(token);
+            if (decodedToken.exp * 1000 < Date.now()) {
+                localStorage.removeItem("token");
+                setIsLoggedIn(false); // Déconnectez l'utilisateur si le token est expiré
+            }
+        }
+    };
+
+    useEffect(() => {
+        hasTokenExpired();
+
+        // Vérifie l'expiration du token toutes les x millisecondes (par exemple toutes les 10 secondes)
+        const interval = setInterval(() => {
+            hasTokenExpired();
+        }, 3 * 60 * 60 * 1000); //3*60/minute 60/seconde et 10000 ms = 10 secondes
+
+        return () => {
+            clearInterval(interval); // Nettoie l'intervalle lorsque le composant est démonté
+        };
+    }, []);
     // Crée une valeur de contexte en utilisant useMemo pour éviter les re-créations inutiles
     const providerValue = useMemo(() => ({
         isLoggedIn,
@@ -28,7 +52,7 @@ export const AuthProvider = ({ children }) => {
     }, [refreshPage]);
     return (
         <AuthContext.Provider value={providerValue}>
-            { children }
+            {children}
         </AuthContext.Provider>
     );
 };
